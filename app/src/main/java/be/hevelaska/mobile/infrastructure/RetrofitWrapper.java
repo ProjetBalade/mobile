65cffdf46f59e7b52f218d2e6e9138173291f050
hevelaska <55829442+hevelaska@users.noreply.github.com>
2021-12-20 11:43:20 +0100

    changement ip

--- app/src/main/java/be/hevelaska/mobile/infrastructure/RetrofitWrapper.java
+++ app/src/main/java/be/hevelaska/mobile/infrastructure/RetrofitWrapper.java
@@ -4,7 +4,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitWrapper {
     //private static final String BASE_URL = "http://10.0.2.2:5000/api/";
     // pour l'app sur mon telephone
-    private static final String BASE_URL = "http://10.0.2.2:5000/api/";
+    private static final String BASE_URL = "http://192.168.0.11:8080/api/";
 
     private static retrofit2.Retrofit instance;
 
